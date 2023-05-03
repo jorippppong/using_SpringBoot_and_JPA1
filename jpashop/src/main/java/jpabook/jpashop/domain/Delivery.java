aef35d28ee5c1c4ca759ac0aaffdd52e3a2d3bc6
jorippppong <duelee75@gmail.com>
2023-05-04 00:39:51 +0900

    Section2_3: 엔티티 클래스 개발1

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)  //ordinal 절대 쓰면 안됨.(상수로 지정해서 중간에 값 추가하면 다 밀려서 망함, 꼭 STRING 으로 하지 )
+    private DeliveryStatus status;  //READY, COMP
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,40 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;  //주문 시간 (java 에서 import 받아옴)
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;  //주문상세 [ORDER, CANCEL]
+
+
+
+
+
+}
